cf5b397a09fd5a058f6bf25026f347638ef9ac9a
Thien Vo <thien.vh21@gmail.com>
2023-07-26 14:12:59 +0700

    commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,29 +1,42 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
 import MyAvatar from "./MyAvatar";
 import TanAvatar from "./TanAvatar";
 import FuAvatar from "./FuAvatar";
 import ThangAvatar from "./ThangAvatar";
 import ThuanAvatar from "./ThuanAvatar";
+import ThuanGirlAvatar from "./ThuanGirlAvatar";
 
 export const Experience = () => {
     return (
         <>
             <OrbitControls />
-            <group position-y={-1} position-x={-3} position-z={1}>
-                <MyAvatar />
+            <Sky />
+            <Environment preset="sunset" />
+            <group>
+                <group position-y={-1} position-x={-3} position-z={1}>
+                    <MyAvatar />
+                </group>
+                <group position-y={-1} position-x={-1}>
+                    <FuAvatar />
+                </group>
+                <group position-y={-1} position-x={0} position-z={1}>
+                    <ThangAvatar />
+                </group>
+                <group position-y={-1} position-x={1}>
+                    <TanAvatar />
+                </group>
+                <group position-y={-1} position-x={2} position-z={1}>
+                    <ThuanGirlAvatar />
+                </group>
             </group>
-            <group position-y={-1} position-x={-1}>
-                <FuAvatar />
-            </group>
-            <group position-y={-1} position-x={0} position-z={1}>
-                <ThangAvatar />
-            </group>
-            <group position-y={-1} position-x={1}>
-                <TanAvatar />
-            </group>
-            <group position-y={-1} position-x={2} position-z={1}>
-                <ThuanAvatar />
-            </group>
+            {/* <mesh scale={[]} rotation-x={-Math.PI * 0.5}>
+                <planeGeometry />
+                <meshStandardMaterial color="white" />
+            </mesh> */}
+            <mesh scale={[10, 0.5, 10]} position-y={-1.25}>
+                <boxGeometry />
+                <meshStandardMaterial color="white" />
+            </mesh>
             <ambientLight intensity={1} />
         </>
     );
