5b356f600617785a4f80a2bb4399e344e1131590
Thien Vo <thien.vh21@gmail.com>
2023-07-26 14:38:40 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 
+
 function App() {
+    
     return (
-        <Canvas shadows camera={{ position: [1, 3, 15], fov: 20 }}>
+        <Canvas shadows camera={{ position: [1, 3, 10], fov: 20 }}>
             <color attach="background" args={["#ececec"]} />
             <Experience />
         </Canvas>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,41 +1,43 @@
-import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Environment, OrbitControls, Sky, useTexture } from "@react-three/drei";
 import MyAvatar from "./MyAvatar";
 import TanAvatar from "./TanAvatar";
 import FuAvatar from "./FuAvatar";
 import ThangAvatar from "./ThangAvatar";
 import ThuanAvatar from "./ThuanAvatar";
 import ThuanGirlAvatar from "./ThuanGirlAvatar";
+import { useThree } from "@react-three/fiber";
+import { MeshBasicMaterial } from "three";
 
 export const Experience = () => {
+    const texture = useTexture("textures/bg1.jpg");
+    const viewport = useThree((state) => state.viewport);
     return (
         <>
             <OrbitControls />
             <Sky />
             <Environment preset="sunset" />
-            <group>
-                <group position-y={-1} position-x={-3} position-z={1}>
-                    <MyAvatar />
-                </group>
-                <group position-y={-1} position-x={-1}>
-                    <FuAvatar />
-                </group>
-                <group position-y={-1} position-x={0} position-z={1}>
-                    <ThangAvatar />
-                </group>
-                <group position-y={-1} position-x={1}>
-                    <TanAvatar />
-                </group>
-                <group position-y={-1} position-x={2} position-z={1}>
-                    <ThuanGirlAvatar />
-                </group>
+            <group position-y={-1} position-x={-3} position-z={1}>
+                <MyAvatar />
             </group>
-            {/* <mesh scale={[]} rotation-x={-Math.PI * 0.5}>
-                <planeGeometry />
-                <meshStandardMaterial color="white" />
-            </mesh> */}
-            <mesh scale={[10, 0.5, 10]} position-y={-1.25}>
+            <group position-y={-1} position-x={-1}>
+                <FuAvatar />
+            </group>
+            <group position-y={-1} position-x={0} position-z={1}>
+                <ThangAvatar />
+            </group>
+            <group position-y={-1} position-x={1}>
+                <TanAvatar />
+            </group>
+            <group position-y={-1} position-x={2} position-z={1}>
+                <ThuanGirlAvatar />
+            </group>
+            <mesh scale={[8, 0.5, 7]} position-y={-1.25} position-z={1}>
                 <boxGeometry />
                 <meshStandardMaterial color="white" />
+            </mesh>
+            <mesh position-z={-2.5} position-y={0.9}>
+                <planeGeometry args={[viewport.width, viewport.height]} />
+                <meshBasicMaterial map={texture} />
             </mesh>
             <ambientLight intensity={1} />
         </>
